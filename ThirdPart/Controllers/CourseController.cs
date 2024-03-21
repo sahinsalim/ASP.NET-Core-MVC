1b885a098d008bae7f9e329fbfb3a2ef9024cf07
Salim Sahin <salimshn02@gmail.com>
2024-03-21 17:24:25 +0300

    Fundamentals and Overview complete.

---
+++ ThirdPart/Controllers/CourseController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ThirdPart.Models;
+
+namespace ThirdPart.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = Repository.Applications;
+            return View(model);
+            //return View();
+        }
+
+        public IActionResult Apply()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Apply([FromForm] Candidate model)
+        {
+            if (Repository.Applications.Any(c=> c.Email.Equals(model.Email)))
+            {
+                ModelState.AddModelError("", "There is already aplication for this user.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                Repository.Add(model);
+                return View("Feedback", model);
+            }
+            return View();
+            
+        }
+    }
+}
---
+++ ThirdPart/Models/Candidate.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ThirdPart.Models
+{
+    public class Candidate
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        public String? Email { get; set; }
+        
+        [Required(ErrorMessage = "FirstName is required.")]
+        public String? FirstName { get; set; }
+        
+        [Required(ErrorMessage = "LastName is required.")]
+        public String? LastName { get; set; }
+        
+        public String? FullName => $"{FirstName} {LastName?.ToUpper()}";
+        public int? Age { get; set; }
+        public String? SelectedCourse { get; set; }
+        public DateTime ApplyAt { get; set; }
+
+        public Candidate()
+        {
+            ApplyAt = DateTime.Now;
+        }
+
+    }
+}
